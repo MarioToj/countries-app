5a33d5d6fc0a87e14a1f8d904d7a6bcd2dac2d9d
Mario Enrique <etalvarez03@gmail.com>
2024-04-30 13:06:05 -0600

    Subiendo mi archivo build

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService ) {}
+
+  searchByCapital(term: string): void {
+    
+    this.countriesService.searchByCapital(term)
+      .subscribe(country => {
+        this.countries = country;
+      })
+
+  }
+
+}
